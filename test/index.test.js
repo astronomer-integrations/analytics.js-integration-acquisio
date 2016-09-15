319407d678bfeacbbfef578a818de359b92d368d
Alois Barreras <alois@vicino.io>
2016-09-14 22:19:44 -0400

    removed unused code from tests, updated gitignore to ignore coverage folder made by karma

--- test/index.test.js
+++ test/index.test.js
@@ -5,23 +5,3 @@ var integration = require('@segment/analytics.js-integration');
 var sandbox = require('@segment/clear-env');
 var tester = require('@segment/analytics.js-integration-tester');
 var MyIntegration  = require('../lib/');
-
-describe('analytics.js-integration-acquisio', function () {
-    var analytics;
-    var myIntegration;
-
-    beforeEach(function () {
-        analytics = new Analytics();
-        myIntegration = new MyIntegration(options);
-        analytics.use(myIntegration);
-        analytics.use(tester);
-        analytics.add(myIntegration);
-    });
-
-    afterEach(function () {
-        analytics.restore();
-        analytics.reset();
-        myIntegration.reset();
-        sandbox();
-    });
-});
