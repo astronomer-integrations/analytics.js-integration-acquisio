44c10531f469e547da66a314c80f490460ba77a7
Alois Barreras <alois@vicino.io>
2016-09-15 15:23:18 -0400

    updated require statement

--- test/index.test.js
+++ test/index.test.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var Analytics = require('@segment/analytics.js-core').constructor;
+var Analytics = require('@astronomerio/analytics.js-core').constructor;
 var integration = require('@segment/analytics.js-integration');
 var sandbox = require('@segment/clear-env');
 var tester = require('@segment/analytics.js-integration-tester');
