9da3cf341c70d3ed74f47fcde5748aa060bcd0c7
Alois Barreras <alois@vicino.io>
2016-09-14 22:16:17 -0400

    formatting

--- test/index.test.js
+++ test/index.test.js
@@ -7,21 +7,21 @@ var tester = require('@segment/analytics.js-integration-tester');
 var MyIntegration  = require('../lib/');
 
 describe('analytics.js-integration-acquisio', function () {
-  var analytics;
-  var myIntegration;
+    var analytics;
+    var myIntegration;
 
-  beforeEach(function () {
-    analytics = new Analytics();
-    myIntegration = new MyIntegration(options);
-    analytics.use(myIntegration);
-    analytics.use(tester);
-    analytics.add(myIntegration);
-  });
+    beforeEach(function () {
+        analytics = new Analytics();
+        myIntegration = new MyIntegration(options);
+        analytics.use(myIntegration);
+        analytics.use(tester);
+        analytics.add(myIntegration);
+    });
 
-  afterEach(function () {
-    analytics.restore();
-    analytics.reset();
-    myIntegration.reset();
-    sandbox();
-  });
+    afterEach(function () {
+        analytics.restore();
+        analytics.reset();
+        myIntegration.reset();
+        sandbox();
+    });
 });
