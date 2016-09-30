d9e0685b0d0d02d146c2a40fd399665e8666bedd
Alois Barreras <alois@vicino.io>
2016-09-29 22:50:46 -0400

    updated deps, added test

--- test/index.test.js
+++ test/index.test.js
@@ -1,7 +1,49 @@
 'use strict';
 
 var Analytics = require('@astronomerio/analytics.js-core').constructor;
-var integration = require('@segment/analytics.js-integration');
+var integration = require('@astronomerio/analytics.js-integration');
 var sandbox = require('@segment/clear-env');
 var tester = require('@segment/analytics.js-integration-tester');
-var MyIntegration  = require('../lib/');
+var Acquisio  = require('../lib/');
+
+describe('Acquisio', function() {
+  var analytics;
+  var acquisio;
+  var options = {
+  };
+
+  beforeEach(function() {
+    analytics = new Analytics();
+    acquisio = new Acquisio(options);
+    analytics.use(Acquisio);
+    analytics.use(tester);
+    analytics.add(acquisio);
+  });
+
+  afterEach(function() {
+    analytics.restore();
+    analytics.reset();
+    acquisio.reset();
+    sandbox();
+  });
+
+  it('should have the correct settings', function() {
+    analytics.compare(Acquisio, integration('Acquisio')
+                      .global('TTDUniversalPixelApi')
+                      .option('events', '')
+                      .option('advertiserId', ''));
+  });
+
+  describe('loading', function() {
+    it('should load', function(done) {
+      analytics.load(acquisio, done);
+    });
+  });
+
+  describe('after loading', function() {
+    beforeEach(function(done) {
+      analytics.once('ready', done);
+      analytics.initialize();
+    });
+  });
+});
